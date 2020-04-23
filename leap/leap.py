bc86954bd5e6c006a9a731febb9ed74dca71e04f
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-04-23 15:43:16 +0300

    Remove needless parentheses

--- leap/leap.py
+++ leap/leap.py
@@ -11,4 +11,4 @@ div_by_400 = partial(div_by, 400)
 
 
 def leap_year(year):
-    return div_by_4(year) and (not div_by_100(year) or div_by_400(year))
+    return div_by_4(year) and not div_by_100(year) or div_by_400(year)
