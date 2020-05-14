c5613c8ef042b7c2c4a7b93c340b8376296b962d
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-05-14 19:44:43 +0300

    "Series" exercise

---
+++ series/series.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+def slices(series: str, length: int) -> List[str]:
+    if series == "":
+        raise ValueError("empty series")
+    if length <= 0:
+        raise ValueError(f"positive length is expected, got {length}")
+    if length > len(series):
+        raise ValueError(f"length {length} is larger than series '{series}'")
+
+    return [series[i : i + length] for i in range(0, len(series) - length + 1)]
