5c3db3b7af2f6011d0c576c5b1870d52352e34d3
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-05-16 15:02:50 +0300

    "Phone Number" exercise

---
+++ phone-number/phone_number.py
@@ -0,0 +1,18 @@
+import re
+
+PATTERN = r"^1?([2-9][0-9]{2})([2-9][0-9]{2})([0-9]{4})$"
+
+
+class PhoneNumber:
+    def __init__(self, number: str):
+        number = "".join(x for x in number if x.isalnum())
+
+        if match := re.match(PATTERN, number):
+            self.area_code, self.exchange, self.subscriber = match.groups()
+        else:
+            raise ValueError("wrong format")
+
+        self.number = f"{self.area_code}{self.exchange}{self.subscriber}"
+
+    def pretty(self) -> str:
+        return f"({self.area_code}) {self.exchange}-{self.subscriber}"
