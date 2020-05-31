8af2b2940b8b667ce3bbfbd4e67dd8ffee355a20
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-05-31 18:49:03 +0300

    "Anagram" exercise

---
+++ anagram/anagram.py
@@ -0,0 +1,11 @@
+from typing import List
+from collections import Counter
+
+
+def is_anagram(word1: str, word2: str) -> bool:
+    return word1 == word2 and Counter(word1) == Counter(word2)
+
+
+def find_anagrams(reference: str, candidates: List[str]) -> List[str]:
+    reference = reference.lower()
+    return [word for word in candidates if is_anagram(word.lower(), reference)]
