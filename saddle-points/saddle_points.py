0c91785c126e20548632e70c2b18f59732e87166
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-06-08 19:37:53 +0300

    "Saddle point" naive solution

--- saddle-points/saddle_points.py
+++ saddle-points/saddle_points.py
@@ -1,37 +1,21 @@
-from operator import gt, lt
-from functools import partial
-from typing import Iterable, List, Dict, Callable
-
-
-CompareFn = Callable[[int, int], bool]
-
-
-def get_ids(items: Iterable[int], comp: CompareFn) -> List[int]:
-    ids = []
-    base_value = None
-
-    for i, value in enumerate(items):
-        if base_value is None or comp(value, base_value):
-            base_value = value
-            ids = [i]
-        elif value == base_value:
-            ids.append(i)
-
-    return ids
-
-
-get_min_ids = partial(get_ids, comp=lt)
-get_max_ids = partial(get_ids, comp=gt)
+from typing import List, Dict
 
 
 def saddle_points(matrix: List[List[int]]) -> List[Dict[str, int]]:
-    points = []
+    """Find all saddle points in matrix.
 
-    for row in matrix:
-        min_col_ids = get_max_ids(row)
-        for col_id in min_col_ids:
-            min_row_ids = get_min_ids(row[col_id] for row in matrix)
-            for row_id in min_row_ids:
-                points.append({"row": row_id, "column": col_id})
+    Saddle point is
 
-    return points
+    - greater than or equal to every element in its row
+    - and less than or equal to every element in its column.
+    """
+    points: List[Dict[str, int]] = []
+
+    try:
+        for i, row in enumerate(matrix):
+            for j, item in enumerate(row):
+                if max(row) == item & min(row[j] for row in matrix) == item:
+                    points.append({"row": i + 1, "column": j + 1})
+        return points
+    except IndexError:
+        raise ValueError("irregular matrix")
