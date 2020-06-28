66fb97f91e2374ae616fee927edcd91b7b16d5cf
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-06-28 17:18:14 +0300

    WIP: better variable names

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -4,8 +4,8 @@ Factors = Tuple[int, int]
 Report = Tuple[Optional[int], List[Factors]]
 
 
-def is_palindrome(number: int) -> bool:
-    string = str(number)
+def is_palindrome(num: int) -> bool:
+    string = str(num)
     return string == string[::-1]
 
 
@@ -14,26 +14,26 @@ def smallest(max_factor: int, min_factor: int) -> Report:
 
 
 def largest(max_factor: int, min_factor: int) -> Report:
-    return palindrome(min_factor, max_factor, rev=False)
+    return palindrome(min_factor, max_factor, reverse=False)
 
 
-def get_factors(number: int, start: int, stop: int) -> Iterator[Factors]:
-    """Yield all possible number factors from a given range."""
-    for i in range(start, stop + 1):
-        if number % i == 0 and start <= i <= number // i <= stop:
-            yield i, number // i
+def get_factors(num: int, start: int, stop: int) -> Iterator[Factors]:
+    """Yield all possible number pair factors from a given range."""
+    for factor in range(start, stop + 1):
+        if num % factor == 0 and start <= factor <= num // factor <= stop:
+            yield factor, num // factor
 
 
-def is_valid_factor(factor: int, number: int, start: int, stop: int) -> bool:
+def is_valid_factor(factor: int, num: int, start: int, stop: int) -> bool:
     """Is given factor a valid number factor from a given range?"""
-    return number % factor == 0 and start <= number // factor <= stop
+    return num % factor == 0 and start <= num // factor <= stop
 
 
-def palindrome(start, stop, rev: bool = True) -> Report:
+def palindrome(start, stop, reverse: bool = True) -> Report:
     if start > stop:
         raise ValueError("stop is less than start")
 
-    if rev:
+    if reverse:
         candidates = range(start ** 2, stop ** 2 + 1)
     else:
         candidates = range(stop ** 2, start ** 2 - 1, -1)
