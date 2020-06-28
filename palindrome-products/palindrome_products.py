aa9dc16116f52b6af0c8ff4a9fc472d72cc91b6d
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-06-28 17:14:58 +0300

    WIP: introduce helper functions

--- palindrome-products/palindrome_products.py
+++ palindrome-products/palindrome_products.py
@@ -1,6 +1,7 @@
-from typing import List, Tuple, Optional, Generator
+from typing import List, Tuple, Optional, Iterator
 
 Factors = Tuple[int, int]
+Report = Tuple[Optional[int], List[Factors]]
 
 
 def is_palindrome(number: int) -> bool:
@@ -8,32 +9,37 @@ def is_palindrome(number: int) -> bool:
     return string == string[::-1]
 
 
-def smallest(max_factor: int, min_factor: int) -> Tuple[Optional[int], List[Factors]]:
+def smallest(max_factor: int, min_factor: int) -> Report:
     return palindrome(min_factor, max_factor)
 
 
-def largest(max_factor: int, min_factor: int) -> Tuple[Optional[int], List[Factors]]:
+def largest(max_factor: int, min_factor: int) -> Report:
     return palindrome(min_factor, max_factor, rev=False)
 
 
-def get_factors(number: int, min_factor: int, max_factor: int) -> Generator[Tuple[int, int], None, None]:
-    for i in range(min_factor, max_factor + 1):
-        if number % i == 0 and min_factor <= i <= number // i <= max_factor:
+def get_factors(number: int, start: int, stop: int) -> Iterator[Factors]:
+    """Yield all possible number factors from a given range."""
+    for i in range(start, stop + 1):
+        if number % i == 0 and start <= i <= number // i <= stop:
             yield i, number // i
 
 
-def palindrome(min_factor, max_factor, rev: bool = True) -> Tuple[Optional[int], List[Factors]]:
-    if min_factor > max_factor:
-        raise ValueError("min factor can't be greater than max factor")
+def is_valid_factor(factor: int, number: int, start: int, stop: int) -> bool:
+    """Is given factor a valid number factor from a given range?"""
+    return number % factor == 0 and start <= number // factor <= stop
+
+
+def palindrome(start, stop, rev: bool = True) -> Report:
+    if start > stop:
+        raise ValueError("stop is less than start")
 
     if rev:
-        candidates = range(min_factor ** 2, max_factor ** 2 + 1)
+        candidates = range(start ** 2, stop ** 2 + 1)
     else:
-        candidates = range(max_factor ** 2, min_factor ** 2 - 1, -1)
+        candidates = range(stop ** 2, start ** 2 - 1, -1)
 
-    for candidate in candidates:
-        if is_palindrome(candidate):
-            for factor in range(min_factor, max_factor + 1):
-                if candidate % factor == 0 and min_factor <= candidate // factor <= max_factor:
-                    return candidate, list(get_factors(candidate, min_factor, max_factor))
+    for candidate in filter(is_palindrome, candidates):
+        for factor in range(start, stop + 1):
+            if is_valid_factor(factor, candidate, start, stop):
+                return candidate, list(get_factors(candidate, start, stop))
     return None, []
