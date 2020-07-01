63b2b69afbbbd9caf43c51a26306fbac10ee8344
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-01 10:59:17 +0300

    Basic solution

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -1,14 +1,21 @@
 from typing import List
 
 
-def equilateral(sides: List[float]) -> bool:
-    unique_sides = set(sides)
-    return len(unique_sides) == 1 and unique_sides != {0}
+def is_valid(sides: List[float]) -> bool:
+    """Sides are not 0 and the longest one is less than the sum of others."""
+    return set(sides) != {0} and sum(sides) - max(sides) > max(sides)
 
 
-def isosceles(sides):
-    pass
+def equilateral(triangle: List[float]) -> bool:
+    """All sides of an equal length."""
+    return len(set(triangle)) == 1 and is_valid(triangle)
 
 
-def scalene(sides):
-    pass
+def isosceles(triangle: List[float]) -> bool:
+    """At least two sides of the equal length."""
+    return len(set(triangle)) <= 2 and is_valid(triangle)
+
+
+def scalene(triangle: List[float]) -> bool:
+    """All sides of a different length."""
+    return len(set(triangle)) == 3 and is_valid(triangle)
