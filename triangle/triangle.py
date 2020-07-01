49364fcecbadd653e5bcef91b3b165e6e4c5a7b1
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-07-01 10:42:08 +0300

    Fix too generous rename

--- triangle/triangle.py
+++ triangle/triangle.py
@@ -2,8 +2,8 @@ from typing import List
 
 
 def equilateral(sides: List[float]) -> bool:
-    sides = set(sides)
-    return len(sides) == 1 and sides != {0}
+    unique_sides = set(sides)
+    return len(unique_sides) == 1 and unique_sides != {0}
 
 
 def isosceles(sides):
