9a9f33cf3d613262ac3cbb14354faf06b0a7be9c
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-09-01 14:07:35 +0300

    Faster hamming solution

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -2,4 +2,4 @@ def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) != len(strand_b):
         raise ValueError("unequal length")
 
-    return sum(a != b for a, b in zip(strand_a, strand_b))
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
