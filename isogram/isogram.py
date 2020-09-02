44921262409882519afaeb1a0b1183bdcf00dd86
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-09-02 17:26:35 +0300

    Changes after review

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,10 +1,9 @@
 def is_isogram(string: str) -> bool:
     seen = set()
-    for char in string:
-        lower_char = char.lower()
-        if lower_char.isalnum():
-            if lower_char in seen:
+    for char in string.lower():
+        if char.isalpha():
+            if char in seen:
                 return False
             else:
-                seen.add(lower_char)
+                seen.add(char)
     return True
