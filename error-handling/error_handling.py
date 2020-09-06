3fbbcc240a5abbec11752d61f8c65cf8add7138b
Pavel Karateev <lancelote.du.lac@gmail.com>
2020-09-06 19:54:03 +0300

    Simplify solution

--- error-handling/error_handling.py
+++ error-handling/error_handling.py
@@ -1,5 +1,7 @@
 from typing import Optional, Tuple
 
+Result = Tuple[bool, Optional[int]]
+
 
 def handle_error_by_throwing_exception() -> None:
     raise ValueError("unexpected value")
@@ -12,20 +14,11 @@ def handle_error_by_returning_none(input_data: str) -> Optional[int]:
         return None
 
 
-def handle_error_by_returning_tuple(
-    input_data: str,
-) -> Tuple[bool, Optional[int]]:
-    try:
-        return True, int(input_data)
-    except ValueError:
-        return False, None
+def handle_error_by_returning_tuple(input_data: str) -> Result:
+    result = handle_error_by_returning_none(input_data)
+    return result is not None, result
 
 
 def filelike_objects_are_closed_on_exception(filelike_object) -> None:
-    try:
-        filelike_object.open()
-        filelike_object.do_something()
-    except Exception as e:
-        raise e
-    finally:
-        filelike_object.close()
+    with filelike_object as file:
+        file.do_something()
