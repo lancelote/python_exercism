b489ec381b7765a4218c8ffddc54f937c447c3e6
Pavel Karateev <lancelote.du.lac@gmail.com>
2021-01-16 19:54:14 +0300

    "Bob" use built-in isupper

--- bob/bob.py
+++ bob/bob.py
@@ -1,22 +1,9 @@
-import re
-
-
-def all_capitals(phrase: str) -> bool:
-    return bool(
-        not re.search(r"[a-z]", phrase) and re.search(r"[A-Z]", phrase)
-    )
-
-
-def is_nothing(hey_bob: str) -> bool:
-    return len(hey_bob) == 0
-
-
 def response(hey_bob: str) -> str:
     phrase = hey_bob.strip()
 
-    if is_nothing(phrase):
+    if len(phrase) == 0:
         return "Fine. Be that way!"
-    if all_capitals(phrase):
+    if phrase.isupper():
         if phrase.endswith("?"):
             return "Calm down, I know what I'm doing!"
         else:
